874859931308308cf75d5b1f72b3f4e70f1c3f04
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-22 23:25:40 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,22 @@
+
+require "pry"
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  #binding.pry 
+  array.count do |num|
+   if num == num.to_s
+    num
+  end 
+  end 
 end
+
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  
+  array.count do |num|
+     if num ==""
+   num
+ end 
+end
 end
